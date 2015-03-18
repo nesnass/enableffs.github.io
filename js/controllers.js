d0fe962d118ab338717da09a5130105dafdbc733
Jeremy Toussaint <jt151077@gmail.com>
2015-03-18 08:32:58 +0100

    - switch localhost

--- js/controllers.js
+++ js/controllers.js
@@ -1,8 +1,15 @@
 enableAppControllers.controller("HomeCtrl", function ($scope, $location, $translate) {
         console.log('--> home started');
 
+        $scope.localmode = false;
+
         //create the global (client) API url based on location object
         options.api.base_url = $location.$$protocol+'://'+$location.$$host+':'+$location.$$port;
+
+        if($location.$$host == 'localhost') {
+            $scope.localmode = true;
+        }
+
 
         $scope.changeLanguage = function (langKey) {
             $translate.use(langKey);
