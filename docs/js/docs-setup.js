16bd70c503f6c33d4dbf8a5d4d0818541a5cb140
Jeremy Toussaint <jt151077@gmail.com>
2015-03-19 14:01:53 +0100

    - added missing doc files

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,26 @@
+NG_DOCS={
+  "sections": {
+    "api": "Docs"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "HomeCtrl",
+      "shortName": "HomeCtrl",
+      "type": "controller",
+      "moduleName": "HomeCtrl",
+      "shortDescription": "Controller for the default partial: home.html",
+      "keywords": "api controller default homectrl html partial"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "angular.js",
+    "angular-animate.js"
+  ]
+};
