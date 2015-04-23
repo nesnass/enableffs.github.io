06c57c41280210eafb01b6c954a5ecbb43784953
Jeremy Toussaint <jt151077@gmail.com>
2015-04-23 14:10:51 +0200

    - local video player

--- js/directives.js
+++ js/directives.js
@@ -2,8 +2,27 @@
  * Created by jeremyt on 22/04/15.
  */
 
-enableAppDirectives.directive('focusMe', function() {
-    return function(scope, element, attrs) {
-        element.attr("tabIndex", -1).focus();
-    };
-});
+enableAppDirectives.directive('autoActive', ['$location', function ($location) {
+    return {
+        restrict: 'A',
+        scope: false,
+        link: function (scope, element) {
+            function setActive() {
+                var path = $location.path();
+                if (path) {
+                    angular.forEach(element.find('a'), function (a) {
+                        if (a.href.match('#' + path + '(?=\\?|$)')) {
+                            angular.element(a).addClass('sidenavlinksactive');
+                        } else {
+                            angular.element(a).removeClass('sidenavlinksactive');
+                        }
+                    });
+                }
+            }
+
+            setActive();
+
+            scope.$on('$locationChangeSuccess', setActive);
+        }
+    }
+}]);
