3f5041fc447a9304408812d4ecda3728e84b3137
Jeremy Toussaint <jt151077@gmail.com>
2015-05-06 11:11:13 +0200

    - youtube video directive

--- js/directives.js
+++ js/directives.js
@@ -37,12 +37,24 @@ enableAppDirectives.directive('enableYoutube', function($sce) {
         restrict: 'AE',
         replace: 'true',
         template: '<div id="youtubevideo">' +
-                    '<h2>Video loaded from youtube</h2>' +
-                    '<span class="screenReadersOnly">For a fully keyboard-accessible alternative to this video, view it in Chrome or on any Android or iOS device, view it in Firefox with the YouTube ALL HTML5 add-on installed, or disable Flash in Internet Explorer.</span>' +
+                    '<h2>{{header}}</h2>' +
+                    '<span class="screenReadersOnly">{{blindMessage}}</span>' +
                     '<div class="videoWrapper">' +
                     '<iframe aria-label="Youtube video iframe" width="640" height="360" src="{{vidurl}}" frameborder="0" allowfullscreen></iframe></div></div>',
         link: function(scope) {
             scope.vidurl = $sce.trustAsResourceUrl("http://www.youtube.com/embed/"+scope.vidid+"?html5=1&controls=1&autohide=0&rel=0&showinfo=0&hl="+scope.cclang+"&cc_load_policy=1");
+
+            switch(cclang) {
+                case 'en':
+                    scope.header = "Video loaded from youtube";
+                    scope.blindMessage = "For a fully keyboard-accessible alternative to this video, view it in Chrome or on any Android or iOS device, view it in Firefox with the YouTube ALL HTML5 add-on installed, or disable Flash in Internet Explorer.";
+                    break;
+                case 'fr':
+                    scope.header = "Video loaded from youtube";
+                    scope.blindMessage = "Pour une alternative entièrement accéssible au clavier, charger la page avec Chrome ou avec m'importe quel appareil Android ou iOS, avec Firefox et le ALL HTML5 module installé, ou bien en annulant Flash sous Internet Explorer.";
+                    break;
+
+            }
         }
     };
 });
